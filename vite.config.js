eee0138aaf77a204100bb14f11f69b84639465ca
Rahul Sugumar <rahulsugumar1977@gmail.com>
2026-03-31 23:48:31 +0530

    Created Deploy.yml file

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './', // Use relative paths for production
   plugins: [
     tailwindcss(),
     react(),
